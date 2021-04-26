aa2ca867be3f00d4ef8d075c2e84a26af369c94f
Matheus de Oliveira <75751795+mattoliv3r@users.noreply.github.com>
2021-04-26 15:51:21 -0300

    Initial commit

---
+++ app/src/main/java/sp/senai/br/mercapli/MainActivity.java
@@ -0,0 +1,24 @@
+package sp.senai.br.mercapli;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
