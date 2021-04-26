5b9ba326b2fbf0c32119495d8bf6a5f8e1275d21
Matheus de Oliveira <75751795+mattoliv3r@users.noreply.github.com>
2021-04-26 16:47:49 -0300

    Criação da Activity_carrinho e Layout_produto
    (Não estão conectados e não estão funcionando)

---
+++ app/src/main/java/sp/senai/br/mercapli/CarrinhoActivity.java
@@ -0,0 +1,14 @@
+package sp.senai.br.mercapli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarrinhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrinho);
+    }
+}
