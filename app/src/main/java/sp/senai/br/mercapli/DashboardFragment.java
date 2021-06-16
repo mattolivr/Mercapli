9d4ff570ef1fb0a7058634634b25a288b4902d4d
Matheus de Oliveira <75751795+mattoliv3r@users.noreply.github.com>
2021-06-16 10:08:10 -0300

    Atalhos do Dashboard Fragment

--- app/src/main/java/sp/senai/br/mercapli/DashboardFragment.java
+++ app/src/main/java/sp/senai/br/mercapli/DashboardFragment.java
@@ -1,10 +1,12 @@
 package sp.senai.br.mercapli;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -23,6 +25,7 @@ import java.text.DecimalFormat;
 
 import sp.senai.br.mercapli.components.ProgressBarMeta;
 import sp.senai.br.mercapli.database.CriarBD;
+import sp.senai.br.mercapli.dialogs.MetaDialog;
 
 import static sp.senai.br.mercapli.GlobalVariables.GASTO_TOTAL;
 import static sp.senai.br.mercapli.GlobalVariables.META_GASTOS;
@@ -32,6 +35,7 @@ public class DashboardFragment extends Fragment {
     private TextView tvGastoTotal;
     private DrawerLayout drawerLayoutDashboard;
     private ImageButton imageMenuDashboard;
+    private ImageButton ibAtalhoCompra, ibAtalhoLista, ibAtalhoMeta, ibAtalhoConfig;
     private NavigationView navigationViewDashboard;
     private NavController navControllerDashboard;
     private ProgressBarMeta pbMeta;
@@ -58,18 +62,27 @@ public class DashboardFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
-        drawerLayoutDashboard = view.findViewById(R.id.drawerLayoutDashboard);
-        imageMenuDashboard = view.findViewById(R.id.ibDashMenu);
-        tvGastoTotal = view.findViewById(R.id.tvDashValorTotal);
+        drawerLayoutDashboard   = view.findViewById(R.id.drawerLayoutDashboard);
+        imageMenuDashboard      = view.findViewById(R.id.ibDashMenu);
+        tvGastoTotal            = view.findViewById(R.id.tvDashValorTotal);
         navigationViewDashboard = view.findViewById(R.id.navigationViewDashboard);
+        ibAtalhoCompra          = view.findViewById(R.id.ibDashAtalhoAddCompra);
+        ibAtalhoLista           = view.findViewById(R.id.ibDashAtalhoAddLista);
+        ibAtalhoMeta            = view.findViewById(R.id.ibDashAtalhoAddMeta);
+        ibAtalhoConfig          = view.findViewById(R.id.ibDashAtalhoConfig);
 
         navControllerDashboard = Navigation.findNavController(getActivity(), R.id.fragment);
 
         database = new CriarBD(view.getContext()).getReadableDatabase();
         pbMeta = new ProgressBarMeta(view, R.id.pbDashMeta);
 
-        imageMenuDashboard.setOnClickListener(view1 -> drawerLayoutDashboard.openDrawer(GravityCompat.START));
         NavigationUI.setupWithNavController(navigationViewDashboard, navControllerDashboard);
+
+        imageMenuDashboard.setOnClickListener(menu -> drawerLayoutDashboard.openDrawer(GravityCompat.START));
+        ibAtalhoCompra.setOnClickListener(compra -> callCompraActivity());
+        ibAtalhoLista .setOnClickListener(lista  -> callListaActivity());
+        ibAtalhoMeta  .setOnClickListener(meta   -> callMetaDialog());
+        ibAtalhoConfig.setOnClickListener(cnfg   -> callConfigActivity());
 
         atualizarProgressoMeta();
         return view;
@@ -89,5 +102,25 @@ public class DashboardFragment extends Fragment {
     private void atualizarProgressoMeta(){
         int valorRestante = META_GASTOS.getValorRestantePorcentagem();
         pbMeta.atualizar(valorRestante);
+    }
+
+    private void callCompraActivity(){
+        Intent it = new Intent(this.getContext(), CarrinhoActivity.class);
+        startActivity(it);
+    }
+
+    private void callListaActivity(){
+        Intent it = new Intent(this.getContext(), ListaActivity.class);
+        startActivity(it);
+    }
+
+    private void callMetaDialog(){
+        DialogFragment dfMeta = new MetaDialog(database);
+        dfMeta.show(getParentFragmentManager(), "metaAtalho");
+    }
+
+    private void callConfigActivity(){
+        Intent it = new Intent(this.getContext(), ConfigFragment.class);
+        startActivity(it);
     }
 }
